a4eb1e8a65a14a0aa8cd04a58ce90bafb27f67c6
Iñaki Arenaza <iarenaza@mondragon.edu>
2014-03-11 16:37:14 +0100

    Add support for multiple clones of the LDAP auth plugin
    Signed-off-by: Iñaki Arenaza <iarenaza@mondragon.edu>

--- auth/ldap/version.php
+++ auth/ldap/version.php
@@ -25,6 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$authtype = 'ldap';
+
 $plugin->version   = 2014111000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014110400;        // Requires this Moodle version
 $plugin->component = 'auth_ldap';       // Full name of the plugin (used for diagnostics)
+
